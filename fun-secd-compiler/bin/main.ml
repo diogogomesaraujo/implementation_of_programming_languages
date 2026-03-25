f77b260b845a364efb6f305c9059c11c19a731b7
Diogo Gomes de Araújo <diogoaraujo2004@gmail.com>
2026-03-25 12:12:55 +0000

    report

--- fun-secd-compiler/bin/main.ml
+++ fun-secd-compiler/bin/main.ml
@@ -3,7 +3,7 @@ open Fun.Run
 open Fun.Parse
 
 let () =
-  let fib = parse_from_file "examples/catalan.fn" |> Option.get in
+  let fib = parse_from_file "examples/incr.fn" |> Option.get in
   match compile fib [] |> run with
   | Int x -> Printf.printf "%d\n" x
   | _ -> failwith "Couldn't reach an integer value."
